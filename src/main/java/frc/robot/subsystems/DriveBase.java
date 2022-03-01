085f4362fe8b41ea9e6d65887320167b07a35c22
Richard Link <rlink@linkdheartsranch.com>
2022-03-01 13:38:38 -0800

    added back in turbo

--- src/main/java/frc/robot/subsystems/DriveBase.java
+++ src/main/java/frc/robot/subsystems/DriveBase.java
@@ -70,8 +70,8 @@ public class DriveBase extends SubsystemBase {
 
     if(RobotState.isEnabled() && RobotState.isTeleop()){
       if(RobotContainer.DriveDirection==1){
-        leftMaster.set(TalonFXControlMode.PercentOutput, Tools.featherJoystick(RobotContainer.DriverController.getRightY(), Constants.JoystickSensitivity));
-        rightMaster.set(TalonFXControlMode.PercentOutput, -1 * Tools.featherJoystick(RobotContainer.DriverController.getLeftY(), Constants.JoystickSensitivity));
+        leftMaster.set(TalonFXControlMode.PercentOutput, DrivePowerModifer * Tools.featherJoystick(RobotContainer.DriverController.getRightY(), Constants.JoystickSensitivity));
+        rightMaster.set(TalonFXControlMode.PercentOutput, -1 * DrivePowerModifer * Tools.featherJoystick(RobotContainer.DriverController.getLeftY(), Constants.JoystickSensitivity));
       }else{
         leftMaster.set(TalonFXControlMode.PercentOutput, (-1 * DrivePowerModifer * Tools.featherJoystick(RobotContainer.DriverController.getLeftY(), Constants.JoystickSensitivity)));
         rightMaster.set(TalonFXControlMode.PercentOutput, (DrivePowerModifer * Tools.featherJoystick(RobotContainer.DriverController.getRightY(), Constants.JoystickSensitivity)));
